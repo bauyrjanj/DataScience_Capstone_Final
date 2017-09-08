163424b8c0c5d72e631f8b06bd600c829e6ea7df
Bauyrjan Jyenis <b.jyenis@gmail.com>
2017-09-08 11:36:38 -0700

    First commit

---
+++ server.R
@@ -0,0 +1,47 @@
+options(shiny.maxRequestSize=30*1024^2)
+
+library(shiny)
+library(data.table)
+library(NLP)
+library(tm)
+library(qdap)
+library(stringr)
+
+# Loading unigram, bigram, and trigram frequency lists
+load("nfreq.v5.RData")
+message<-""
+## Function that predicts the next word
+predicWord<-function(txt.in){
+        count<-as.numeric(wc(txt.in))
+        if (count==2) {
+                l<-ngramsList[[3]]
+                itx<-word(txt.in, start=2, sep = " ")
+                p=grep(itx, l$w2)
+                nxt<-as.character(l$w3[p])
+                message<<-"Predicted the NEXT word using Trigram"
+                return(nxt[1])
+                
+        } else if (count==1) {
+                l<-ngramsList[[2]]
+                itx<-word(txt.in, start=1, sep = " ")
+                p=grep(itx, l$w1)
+                nxt<-as.character(l$w2[p])
+                message<<-"Predicted the NEXT word using Bigram"
+                return(nxt[1])
+        } else {
+                l<-ngramsList[[1]]
+                nxt<-l$w[1]
+                message<<-"Predicted the NEXT word using Unigram"
+                return(nxt[1])
+        }
+}
+
+
+# Define server logic required to summarize and view the selected dataset
+shinyServer(function(input, output) {
+            output$word_next<-renderPrint({
+                    result<-predicWord(input$obs)
+                    output$sentence<- renderText({message})
+                    result
+              })
+})
---
+++ ui.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+        
+        # Application title.
+        titlePanel("DATA SCIENCE CAPSTONE PROJECT - PREDICT NEXT WORD"),
+        sidebarLayout(
+                sidebarPanel(
+                        helpText("ENTER A WORD, OR SET OF WORDS TO GET THE NEXT WORD PREDICTED"),
+                        hr(),
+                        textInput("obs", "Enter Your Input Text Here:"),
+                        submitButton("Submit"),
+                        hr(),
+                        helpText("1 - ONCE THE INPUT IS ENTERED AND SUBMITTED, THE NEXT WORD IS PREDICTED AND OUTPUTTED.", 
+                                 hr(),
+                                 "2 - YOUR INPUT MAY BE A SINGLE WORD OR SET OF WORDS UP TO THREE WORDS.",
+                                 hr(),
+                                 "3 - THIS APP DOES NOT SUPPORT THE INPUT WHERE NUMBER OF WORDS IS GREATER THAN THREE",
+                                 hr(),
+                                 "4 - THE FUTURE IMPROVED VERSION OF THE APP WILL BE SCALED TO WORK WELL WITH THE NUMBER OF WORDS GREATER THAN THREE"),
+                        hr(),
+                        hr()
+                        
+                ),
+                mainPanel(
+                        h3("NEXT WORD:"),
+                        textOutput("word_next"),
+                        hr(),
+                        strong("Please ignore the error message 'missing value where TRUE/FALSE needed'"),
+                        strong(code(textOutput('sentence'))),
+                        hr(),
+                        helpText("THE PREDICTION MODEL IS BASED ON KATZ-BACK OFF ALGORITHM"),
+                        hr()
+                )
+        )
+))
